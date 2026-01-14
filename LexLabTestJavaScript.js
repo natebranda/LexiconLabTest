8c8ae4649df75e09dd610b6642f8d3ed69d31a8b
natebranda <natebranda@icloud.com>
2026-01-14 17:00:59 -0500

    Reformatted the tutorial stimulus and got prime words feature working as specified in the excercise.

--- LexLabTestJavaScript.js
+++ LexLabTestJavaScript.js
@@ -7,12 +7,19 @@
  * @author Nathaniel Branda
  */
 
-// GLOBAL variables:
+// GLOBAL variables://////////////REMOVE and make part of trial///////////////////////////////////////////////////////////////////
 
 // Saves the start time of the current trial so that
 // the subit times of words can be assigned 
 // relative to when their specific trial starts. 
-current_trial_start_time = performance.now();
+let current_trial_start_time = performance.now();
+
+// Tracks how many responses are left until the
+// next prime word should be displayed. Its 
+// initial value is assigned at the beginning of
+// a trial and when a new prime word appears. 
+let responses_until_prime_word;
+
 
 // initialize jsPsych library and object
 const jsPsych = initJsPsych({
@@ -23,36 +30,99 @@ const jsPsych = initJsPsych({
 });
 
 /**
- * Prepares textarea input box for more text and 
- * for submitting words with enter by giving it 
- * an event listener. Moreover, it initializes 
- * the desired data objects for the current trial.
+ * This function adds the contents of the input box
+ * and the time they were submitted to the words list.
+ * It then clears the input box to make way for further
+ * input.
+ * This function also checks to see if it is time to make
+ * a prime word appear and calls the function to make it appear
+ * if so. Moreover, it handles removing prime words that the 
+ * user has already submitted. 
  * 
- * @param {string} textarea_id The object id of the textarea element
+ * @param {HTMLElement} input_box The textarea box the user
+ * types and submits answers in.
+ * @param {Object[]} words_list The list of all words submitted
+ * in this trial.
+ * @param {string} prime_word_id The object id of the prime word text element.
+ * @param {string[]} prime_words The list of all prime words in this trial
+ */
+function submit_word(input_box, words_list, prime_word_id, prime_words) {
+    //get string contents of inputs
+    contents = input_box.value;
+
+    //if the input is not empty
+    if (contents != "") {
+        //mark that a response has occurred.
+        responses_until_prime_word--;
+        //get time since start of trial
+        current_time = performance.now() - current_trial_start_time;
+        //add submitted word to data
+        words_list.push({word: contents, time: current_time});
+        //clear input box text
+        input_box.value = "";
+
+        // Put contents in format of strings in prime words list
+        mod_contents = contents.trim().toUpperCase();
+        // check for submitted word in list of prime words
+        const index = prime_words.indexOf(mod_contents);
+        if (index > -1) {
+            // delete it if found
+            prime_words.splice(index, 1);
+        }
+
+        // If it is time for a prime word to appear AND if unguessed prime words remain ////////////////////////////////////
+        if ((responses_until_prime_word == 0) && (prime_words.length > 0)) {
+
+            // get prime word HTML text elements
+            display_element = jsPsych.getDisplayElement();
+            var prime_word_text = display_element.querySelector(prime_word_id);
+
+            //get random remaining prime word
+            random_prime_word = prime_words[Math.floor(Math.random() * prime_words.length)];
+            // display word
+            prime_word_text.innerHTML = random_prime_word;
+            // disable ability to type in input box
+            input_box.disabled = true;
+            //reset responses until prime word to a number between 3 and 7
+            responses_until_prime_word = Math.floor((Math.random() * 4) + 3);
+
+            // wait 500 milliseconds
+            setTimeout(() => {
+                //then, remove prime word by setting it to empty string
+                prime_word_text.innerHTML = "";
+                //reactivate input box
+                input_box.disabled = false;
+            }, 500)
+        }
+    }
+}
+
+/**
+ * Prepares textarea input box for submitting words
+ * with the Enter key by adding an event listener to it.
+ * It also sets the responses until next prime word 
+ * variable to a number between 3 and 7 to start the trial.
+ * 
+ * @param {string} input_box_id The object id of the textarea element.
  * @param {Object[]} words_list A list of objects composed of user submitted
  * words and the time they were submitted relative to the start of the trial.
+ * @param {string} prime_word_id The object id of the prime word text element.
+ * @param {string[]} prime_words A list of prime words for the current trial.
  */
-function prepare_new_trial(textarea_id, words_list) {
+function prepare_new_trial(input_box_id, words_list, prime_word_id, prime_words) {
+    // sets below to an integer between 3 and 7 (inclusive)
+    responses_until_prime_word = Math.floor((Math.random() * 4) + 3);
+
     // get contents of input box
-    input_box = document.getElementById(textarea_id);
+    input_box = document.getElementById(input_box_id);
     // record text as it is typed
     input_box.addEventListener('keydown', (event) => {
         // Check if the key pressed is the 'Enter' key
         if (event.key === 'Enter') {
             // Prevent the default function of Enter key press
             event.preventDefault();
-            //get string contents of input
-            contents = input_box.value;
-
-            //if the input is not empty
-            if (contents != "") {
-                //else, get time since start of trial
-                current_time = performance.now() - current_trial_start_time;
-                //add submitted word to data
-                words_list.push({word: contents, time: current_time});
-                //clear input box text
-                input_box.value = "";
-            }
+            //submit contents to words list
+            submit_word(input_box, words_list, prime_word_id, prime_words);
         } 
     });
 }
@@ -64,17 +134,21 @@ var tutorial = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `
         <h2 style="margin-bottom: 10px; text-align: center; font-size: 40px">
-            Category Words will appear here
+            Category Words will appear here.
         </h2>
 
         <h3 style="margin-bottom: 10px; text-align: center; color:blue; font-size: 30px">
-            Prime Words will appear here. You will not be able to type when they are on screen.
+            Prime Words will appear here. <br> 
+            You will not be able to type when they are on screen. <br>
+            Remember: you will need to click the text box again to continue typing after a prime
+            word appears. 
         </h3>
 
         <textarea id="input-box" rows="1" cols="100" autofocus></textarea>
         
         <p style="margin-bottom: 10px; text-align: center; font-size: 30px">
-            The text box above is where you will type your responses.
+            The text box immediately above is where you will type single word responses to 
+            the aformentioned Category Words. <br>
             Type a word and press ENTER to submit it. 
         </p>
 
@@ -89,15 +163,15 @@ var animals = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `
         <h2 style="margin-bottom: 10px; text-align: center; font-size: 40px">
-            Category: Animals
+            Category: ANIMALS
         </h2>
         <h3 id="prime_word" style="margin-bottom: 10px; text-align: center; color:blue; font-size: 30px">
-            -
+            
         </h3>
         <textarea id="input_box" rows="1" cols="100" autofocus></textarea>
     `,
     //key presses do not end the trial, only the timer
-    choices: ['0'], //"NO_KEYS", ////////////////////////////////////////////////////////////////////////////////////////////////
+    choices: ["NO_KEYS"], 
     trial_duration: 60000, // 60 second trial
 
     on_load: function() {
@@ -105,16 +179,25 @@ var animals = {
         current_trial_start_time = performance.now();
         //declare temporary holder for submitted words
         words_list = [];
-        prepare_new_trial('input_box', words_list);
 
-        // mark duration of trial for later use
-        //const trial_duration = 60000;         
-        // Get prime word element
-        //display_element = jsPsych.getDisplayElement();
-        //var prime_word_text = display_element.querySelector('#prime_word');
+        // set up Prime Words list 
+        // (NOTE: for a full version I would include a more exhaustive list)
+        const prime_words = [
+            "WALRUS",
+            "ALLIGATOR",
+            "CROCODILE",
+            "BEAVER",
+            "HORSE",
+            "SPIDER",
+            "DOG",
+            "CAT",
+            "PELICAN",
+            "SEAGULL",
+            "EAGLE",
+            "MOUSE"
+        ];
 
-        // set up Prime Words Stack
-        //const prime_words = [];
+        prepare_new_trial('input_box', words_list, '#prime_word', prime_words);
     },
 
     on_finish: function(data) {
